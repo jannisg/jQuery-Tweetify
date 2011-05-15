5ae9898a5eb07a4f8db1b23b3891bd0968468a94
Jannis Gundermann <jannis@gundermann.me>
2011-05-15 20:09:26 +1200

    beginning of the readme. rewitten some bits, currently breaking things

--- jquery.tweetify.js
+++ jquery.tweetify.js
@@ -3,70 +3,72 @@
 	$.fn.extend({
 		tweetify : function( options ) {
 			
-			var privateOptions = {
-				'url' : 'http://twitter.com/share',
-				'buttonClass' : 'twitter-share-button'
+			var internal = {
+				'baseurl' : 'http://twitter.com/share',
+				'buttonClass' : 'twitter-share-button',
+				'attrs' : [ 'url' , 'via' , 'text' , 'related' , 'count' , 'lang' , 'counturl' ]
 			}
-			var options = $.extend( {} , privateOptions , $.fn.tweetify.defaults , options )
+			
+			var options = $.extend( {} , internal , $.fn.tweetify.defaults , options ), o = options;
 			var pageurl = window.location.protocol+'//'+window.location.host+window.location.pathname;
 			var twitterJs = '<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>';
 			
 			this.each(function() {
-				var o = options;
 				var self = $(this);
-
 				// store specific data to include in this button
-				var data = o;
-				
-				// check if the queries.url is blank, if so set it to the current path
-				if ( data.queries.url == "" || typeof data.queries.url !== "string" ) { 
-					 data.queries.url = pageurl;
-				}
-				// if the data-text attribute is available use it.
-				if ( data.queries.text == "" && typeof self.data('text') !== "undefined" ) { 
-					data.queries.text = self.data('text');
-				} else if ( typeof self.data('text') === "undefined" ) {
-					data.queries.text = '';
-				}
+				var data = o, defaultvalues = data.queries, myValues = {};
 
-				// attach custom hook at the end of ones url, eg: for adding a query string.
-				if ( typeof o.urlExtend !== "undefined" ) {
-					// store the selector string.
-					var extension = o.urlExtend;
-					// if not a string make it one.
-					if ( typeof extension !== "string" ) { 
-						 extension = o.urlExtend.toString();
+				$.each( internal.attrs , function(i, val) {
+				  if ( typeof self.data(val) !== "undefined" ) { 
+						myValues[val] = self.data(val);
+				  } else {
+						myValues[val] = defaultvalues[val];
 					}
+				});
+				
+				// loop through existing data-attributes and if they're twitter related override the defaults.
+				$.each( self.data() , function(key, val) {
+					if ( $.inArray( key , internal.attrs ) ) { 
+						myValues[key] = val; // if key is a twitter api attribute then store it, overriding the defaults.
+					}
+				});
+			
+				// attach custom hook at the end of ones url, eg: for adding a query string.
+				if ( typeof self.data('extension') !== "undefined" ) {
+					// store the selector string.
+					var extension = self.data('extension');
+					// if not a string make it one.
+					if ( typeof extension !== "string" ) { extension = self.data('extension').toString(); }
 					// append this sting to the URL
-					data.queries.url += encodeURIComponent( extension );
+					defaultvalues.url += encodeURIComponent( extension );
 				}
 
 				if ( o.customButton ) { 
 					// if we have a custom button to display, don't attach the script and instead create some custom share urls.
 					// generate a encoded URL with all parameters.
 					var generate = '';
-					$.each( data.queries , function(key, val) {
+					$.each( defaultvalues , function( key, val ) {
 						if ( val != '' ) { 
 					  	generate += '&'+key+'='+encodeURIComponent( val );
 						}
 					});
 					var generate = generate.replace( /^&/ , '?'); // make the first ampersand a questionmark to begin the query string that is being attached to the share URL
 					
-					self.attr('href', data.url+generate ).bind('click', function(e) {
+					self.attr('href', internal.baseurl+generate ).bind('click', function(e) {
 					  e.preventDefault();
 						window.open(this.href,'_blank','height=450,width=550');
 					});
 					
 				} else {
 					// generate the data-attributes so that once twitter.js ignites it will pick up on our custom options.
-					$.each( data.queries , function( key , val ) {
+					$.each( defaultvalues , function( key , val ) {
 						if ( val != '' && typeof self.data( key ) === "undefined" ) { 
 							self.attr('data-'+key , val );
 						}
 					});
 					// add the default href
 					// add the button class so that the js picks up on it.
-					self.attr('href', data.url ).addClass( data.buttonClass ); 
+					self.attr('href', myValues.url ).addClass( internal.buttonClass ); 
 				}
 
 			}); // <= end the each loop
@@ -86,10 +88,12 @@
 	$.fn.tweetify.defaults = {
 		'queries' : {
 			'url'			: '',
-			'count' 	: 'none',
 			'via' 		: 'jannisg',
+			'text'		: '',
 			'related' : 'jannisg',
-			'text'		: ''
+			'count' 	: 'none',
+			'lang'		: 'en',
+			'counturl': ''
 		},
 		'customButton' : false
 	}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# jQuery Tweetify
+---
+## Purpose
+A simple script that attaches to user defined elements to make them valid Tweet buttons, either using a custom generated (on the fly) href with a build in popup window script or by utilizing the default Twitter widgets.js file.
+
+---
+## History
+I was working on a project where we had to handover code to a third party that unfortunately didn't really know what they were doing.
+Since we had no choice in the matter we had to foolproof our code to the point where things were mostly automated scripts that this third party couldn't break easily yet allowed for relative customizability down the road.
+
+Out of this necessity I created Tweetify.
+
+---
+## Usage
+There are 3 core scenarios:
+
+
+
+---
+## Todo
+
+* Cross browser testing
+* Nicer demo page
